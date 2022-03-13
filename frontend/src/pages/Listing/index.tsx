eae07e35378406f2a60c37f1ec27a40228d6431b
joellytton <joellytton25@gmail.com>
2022-03-13 14:14:00 -0500

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,15 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import MovieScore from "components/MovieScore";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/request";
 
 function Listing() {
+
+    axios.get(`${BASE_URL}/movies`).then(response => {
+        console.log(response.data);
+    });
+
     return (
         <>
             <Pagination />
