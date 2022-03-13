d28fc558fbd96ea9e47297c7ead62e2c372b988a
joellytton <joellytton25@gmail.com>
2022-03-13 15:07:18 -0500

    useParams

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -5,7 +5,6 @@ import { Movie } from "types/movie";
 type Props = { movie: Movie };
 
 function MovieCard({movie} : Props) {
-
     return (
         <div>
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
